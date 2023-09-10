49c90a8fd6c2e10af1a321066f67a3d91ff96d44
Yasmine C. Fortes <yazzcastrof@gmail.com>
2023-09-09 22:00:05 -0300

    first commit

---
+++ LivrariaAPI/Models/Livro.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LivrariaAPI.Models
+{
+    public class Livro
+    {
+        public string ID { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+        public string Categoria { get; set; }
+        public string Capa { get; set; }
+    }
+}
